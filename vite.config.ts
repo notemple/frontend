186f5170e75fa231e4805fb2cbcbe969d5860d4b
n1la9jan <nilanjanmridha89@gmail.com>
2026-06-04 03:15:58 +0530

    build works

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 import path from 'path';
@@ -15,7 +14,6 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
   ],
   build: {
     chunkSizeWarningLimit: 2000,
